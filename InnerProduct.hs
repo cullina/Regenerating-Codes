6978e4ba32d0cfeaa5c46c9aff0f9f75d830b585
cullina <daniel.cullina@gmail.com>
2010-07-10 03:11:40 -0500

    Moved everything related to inner products and projections to InnerProduct

---
+++ InnerProduct.hs
@@ -0,0 +1,45 @@
+module InnerProduct where
+       
+import MatrixUtil (cols)
+import Data.List (transpose, foldl')
+import Math.Algebra.LinearAlgebra
+
+
+
+sqNorm vector = vector <.> vector
+
+
+projection x = let xt = transpose x 
+                   xi = inverse (x <<*>> xt)
+               in case xi of 
+                 Nothing -> Nothing
+                 Just y  -> Just (xt <<*>> y <<*>> x)
+                  
+projectionError x = case (projection x) of
+  Nothing -> Nothing
+  Just y  -> Just (iMx (cols x) <<->> y)
+
+
+
+
+decompLQ :: (Fractional a) => [[a]] -> [[a]]
+
+decompLQ x = decompLQh x []
+
+decompLQh :: (Fractional a) => [[a]] -> [([a],a)] -> [[a]]
+
+decompLQh [] orthoVecs = reverse $ map fst orthoVecs
+
+decompLQh (x:xs) orthoVecs = let projections  = map (vectorProjection x) orthoVecs
+                                 newOrthoVec  = foldl' (<->) x projections
+                                 newOrthoVecs = (newOrthoVec, sqNorm newOrthoVec) : orthoVecs 
+                             in  decompLQh xs newOrthoVecs
+                             
+
+vectorProjection :: (Fractional a) => [a] -> ([a],a) -> [a]
+
+vectorProjection original target = let targetVec = fst target
+                                       targetLen = snd target
+                                   in  ((original <.> targetVec) / targetLen) *> targetVec
+
+
--- MatrixUtil.hs
+++ MatrixUtil.hs
@@ -1,5 +1,7 @@
 module MatrixUtil 
        (
+         rows,
+         cols,
          mMap,
          stdBasisVector,
          isFullRank,
@@ -91,43 +93,6 @@ isFullRank = not . all (0 ==) . last . rowEchelonForm
 normalize [] = []
 normalize (0:xs) = 0 : normalize xs
 normalize (x:xs) = 1 : (1 / x) *> xs
-
-
-sqNorm vector = vector <.> vector
-
-
-projection x = let xt = transpose x 
-                   xi = inverse (x <<*>> xt)
-               in case xi of 
-                 Nothing -> Nothing
-                 Just y  -> Just (xt <<*>> y <<*>> x)
-                  
-projectionError x = case (projection x) of
-  Nothing -> Nothing
-  Just y  -> Just (iMx (cols x) <<->> y)
-
-
-
-
-decompLQ :: (Fractional a) => [[a]] -> [[a]]
-
-decompLQ x = decompLQh x []
-
-decompLQh :: (Fractional a) => [[a]] -> [([a],a)] -> [[a]]
-
-decompLQh [] orthoVecs = reverse $ map fst orthoVecs
-
-decompLQh (x:xs) orthoVecs = let projections  = map (vectorProjection x) orthoVecs
-                                 newOrthoVec  = foldl' (<->) x projections
-                                 newOrthoVecs = (newOrthoVec, sqNorm newOrthoVec) : orthoVecs 
-                             in  decompLQh xs newOrthoVecs
-                             
-
-vectorProjection :: (Fractional a) => [a] -> ([a],a) -> [a]
-
-vectorProjection original target = let targetVec = fst target
-                                       targetLen = snd target
-                                   in  ((original <.> targetVec) / targetLen) *> targetVec
 
 
 intersectionSpace m1 = map (splitAt (rows m1)) . nullspace . (m1 ++)
