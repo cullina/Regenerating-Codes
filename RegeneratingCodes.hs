3f5829605e3105b2d78bc49ff732c396fa653cef
cullina <daniel.cullina@gmail.com>
2010-07-10 04:00:38 -0500

    Improved organization of RegeneratingCodes

--- RegeneratingCodes.hs
+++ RegeneratingCodes.hs
@@ -14,33 +14,25 @@ data RegenCode field = RegenCode {
   , recoveryCoefficients       :: [[[field]]]
   } deriving (Show)
 
-indexList :: (Int -> Int) -> [Int] -> [Int]
 
-indexList f []     = []
-indexList f (p:ps) = map (flip rem p . f) [0..p-1] ++ map (+ p) (indexList  f ps)
+rotationMatrix a = operationMatrix (+ a)
 
-cycleList :: [Int] -> [[Int]] 
-
-cycleList []     = []
-cycleList (x:xs) = [0..x-1] : mMap (x +) (cycleList xs)
+multiplicationMatrix m = operationMatrix (* m)
 
 
 operationMatrix :: (Num a) => (Int -> Int) -> Int -> Int -> [[a]]
 
-operationMatrix f size = permutationMatrix . indexList f . greedyFactorDecomp size
-
-
-rotationMatrix :: (Num a) => Int -> Int -> [[a]]
-
-rotationMatrix = operationMatrix (+ 1)
-
-multiplicationMatrix m = operationMatrix (* m)
+operationMatrix f size = permutationMatrix . indexList f . greedyDecomp size . findFactors
 
 
 permutationMatrix xs = map (stdBasisVector (length xs)) xs
 
 
-greedyFactorDecomp sum = greedyDecomp sum . findFactors
+indexList :: (Int -> Int) -> [Int] -> [Int]
+
+indexList f []     = []
+indexList f (p:ps) = map (flip rem p . f) [0..p-1] ++ map (+ p) (indexList  f ps)
+
 
 greedyDecomp 0 xs   = []
 greedyDecomp sum xs = let h = head xs
@@ -48,10 +40,13 @@ greedyDecomp sum xs = let h = head xs
 	     	      	 then h : greedyDecomp (sum - h) xs
 		      	 else greedyDecomp sum (tail xs)
 
-findFactors n = filter (\x -> rem n x == 0) [n,n-1..1]
 
-coprimes n = filter (\x -> 1 == gcd n x) [2 .. n-1]
+findFactors n = filter ((0 ==) . (rem n)) [n,n-1..1]
 
+
+coprimes n = filter ((1 ==) . (gcd n)) [2 .. n-1]
+
+--------
 
 cycleToImage :: [[Int]] -> [Int]
 
@@ -61,6 +56,51 @@ singleCycleToImage xs   = pairGen (head xs) (head xs) (tail xs)
     where pairGen first prev []    = [(prev, first)]
     	  pairGen first prev rest  = (prev, head rest) : pairGen first (head rest) (tail rest)
 
+--------
+
+getRotations n columns = map (flip (<<*>>)) $ map (\x -> rotationMatrix x columns n) [1..n-1]
+
+
+applyRotations rotations lostStorage = (lostStorage, map ($ lostStorage) rotations)
+
+--------
+
+testLinearIndependence :: (Fractional a) => Int -> ([[a]], [[[a]]]) -> Bool
+
+testLinearIndependence k = all isFullRank . collectionPossibilities k
+
+
+collectionPossibilities :: (Fractional a) => Int -> ([[a]] , [[[a]]]) -> [[[a]]]
+
+collectionPossibilities k items = map (concat . (fst items :)) $ getCombinations (k - 1) (snd items) 
+
+
+getCombinations :: Int -> [a] -> [[a]]
+
+getCombinations 0 list = [[]]
+getCombinations k list = if length list <= k
+		       	 then [list]
+			 else map ((head list) :) (getCombinations (k - 1) (tail list)) ++
+			      getCombinations k (tail list)
+
+--------
+
+searchForRecovery field numARR storage = let lostStorage             = fst storage
+                                             remainingStorage        = snd storage
+                                             additionalRecoveredRows = genAllNonOverlappingSubspaces field lostStorage numARR
+                                             testedCodes             = map (testRecovery lostStorage remainingStorage) additionalRecoveredRows
+                                         in  filter (not . null . recoveryCoefficients) testedCodes
+                                           
+
+testRecovery lostStorage remainingStorage additionalRecovered = RegenCode lostStorage additionalRecovered (testRecovery1 (lostStorage ++ additionalRecovered) remainingStorage)
+
+
+testRecovery1 x = map (map normalize . snd) . filter (isFullRank . fst) . map unzip . recoveryPossibilities x
+                  
+
+recoveryPossibilities x = listCartesianProductOverList . map (intersectionSpace x)
+
+--------
 
 quotientList fs = map head . getCosets fs
 
@@ -74,60 +114,12 @@ addIfNew :: (Eq a) => [a -> a] -> [[a]] -> a -> [[a]]
 addIfNew fs quotient candidate = let coset = candidate : map ($ candidate) fs
                                  in case quotient of
                                    []       -> [coset]
-                                   q@(x:xs) ->  if any (any (== candidate)) quotient
+                                   (x:xs) ->  if any (any (== candidate)) quotient
                                                 then quotient
                                                 else coset : quotient
 
+--------
 
-
-getPowers :: (Num a) => Int -> [[a]] -> [[[a]]]
-
-getPowers n matrix = genPowersStartingFrom n matrix matrix
-    where genPowersStartingFrom 1 start matrix = [start]
-    	  genPowersStartingFrom n start matrix = 
-    	      let next = matrix <<*>> start
-	      in start : genPowersStartingFrom (n - 1) next matrix
-
-
-getRotations :: (Num a) => Int -> Int -> [[[a]] -> [[a]]]
-
-getRotations n columns = map (flip (<<*>>)) $ getPowers (n - 1) $ rotationMatrix columns n
-
-
-applyRotations rotations lostStorage = (lostStorage, map ($ lostStorage) rotations)
-
-getCombinations :: Int -> [a] -> [[a]]
-
-getCombinations 0 list = [[]]
-getCombinations k list = if length list <= k
-		       	 then [list]
-			 else map ((head list) :) (getCombinations (k - 1) (tail list)) ++
-			      getCombinations k (tail list)
-
-
-testLinearIndependence :: (Fractional a) => Int -> ([[a]], [[[a]]]) -> Bool
-
-testLinearIndependence k = all isFullRank . collectionPossibilities k
-
-
-collectionPossibilities :: (Fractional a) => Int -> ([[a]] , [[[a]]]) -> [[[a]]]
-
-collectionPossibilities k items = map (concat . (fst items :)) $ getCombinations (k - 1) (snd items) 
-
-
-recoveryPossibilities x = listCartesianProductOverList . map (intersectionSpace x)
-
-testRecovery1 x = map (map normalize . snd) . filter (isFullRank . fst) . map unzip . recoveryPossibilities x
-
-testRecovery lostStorage remainingStorage additionalRecovered = RegenCode lostStorage additionalRecovered (testRecovery1 (lostStorage ++ additionalRecovered) remainingStorage)
-
-
-searchForRecovery field numARR storage = let lostStorage             = fst storage
-                                             remainingStorage        = snd storage
-                                             additionalRecoveredRows = genAllNonOverlappingSubspaces field lostStorage numARR
-                                             testedCodes             = map (testRecovery lostStorage remainingStorage) additionalRecoveredRows
-                                         in  filter (not . null . recoveryCoefficients) testedCodes
-                                           
 
 searchForCodes field n k =  let rows        = n - k
                                 columns     = rows * k
@@ -140,9 +132,11 @@ searchForCodes field n k =  let rows        = n - k
                                 realCodes   = filter (not . null) codes
                                 x           = map (storageMatrix . head) realCodes
                                 q1          = quotientList (map (reducedRowEchelonForm .) rotations) x
-                            in  quotientList [reducedRowEchelonForm . (<<*>> (multiplicationMatrix (n - 1) columns n))] q1
+                                q2          = quotientList [reducedRowEchelonForm . (<<*>> (multiplicationMatrix (n - 1) columns n))] q1
+                            in  q1
                                
 
+--------
 
 searchForCodesF3 = searchForCodes f3
 
